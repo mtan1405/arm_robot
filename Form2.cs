fc2ba603dcf137d58f838f3722eb919638f7961a
mtan1405 <cmt047437@gmail.com>
2023-11-09 13:11:39 +0700

    add comment

--- Form2.cs
+++ Form2.cs
@@ -247,7 +247,7 @@ namespace arm_robot
         {
             try
             { 
-            if (caculator.checkJoin(joins[0]))
+            if (caculator.checkJoin(joins[0])) // check whether could compute 
             {
                 step = caculator.getStep(ref currentJoin, joins[0]);
                     getStringSteps(step.step_1, step.step_2, step.step_3); 
