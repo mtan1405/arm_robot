f0db4ee4ee099809ad98d08f0a1bce8ad84a9820
mtan1405 <cmt047437@gmail.com>
2023-11-09 13:41:31 +0700

    merge

--- Form2.cs
+++ Form2.cs
@@ -247,7 +247,7 @@ namespace arm_robot
         {
             try
             { 
-            if (caculator.checkJoin(joins[0])) // check whether could compute 2341ss
+            if (caculator.checkJoin(joins[0])) // check whether could compute 2341ss dàdsfads
             {
                 step = caculator.getStep(ref currentJoin, joins[0]);
                     getStringSteps(step.step_1, step.step_2, step.step_3); 
